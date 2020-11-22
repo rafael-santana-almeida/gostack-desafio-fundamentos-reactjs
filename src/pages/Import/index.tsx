959c23133fd58021c1fe79401c15493d5e54b474
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-11-22 20:24:07 -0300

    Adiciona importação de arquivos csv

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,33 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(uploadedFile =>
+      data.append('file', uploadedFile.file),
+    );
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const formatedFilesData = files.map(file => {
+      const formatedFileData = {
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      };
+
+      return formatedFileData;
+    });
+
+    setUploadedFiles(formatedFilesData);
   }
 
   return (
